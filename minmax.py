7ac78923abd6843e1857885cb196d4873a280a06
flor0 <fruppster@gmail.com>
2019-06-04 20:07:21 +0200

    MACHINE IS UNBEATABLE

--- minmax.py
+++ minmax.py
@@ -1,5 +1,5 @@
 import numpy as np
-import random, time, sys
+import copy
 
 def checktictactoe(spielbrett1, player):
     spielbrett_temp = spielbrett1.copy()
@@ -21,61 +21,6 @@ def checkvollesbrett(brett):
                 return False
     return True
 
-
-def status(brett, player):
-    if checktictactoe(brett.copy(), player):
-        print("WINNER")
-        return 1, None, None  # MAX
-    if checktictactoe(brett.copy(), -player):
-        print("LOSER")
-        return -1, None, None  # MIN
-    if checkvollesbrett(brett.copy()):
-        print("DRAW")
-        return 0, None, None  # MIN
-    return False
-
-debug = []
-'''
-def minimax(brett, player):
-    # Check if you can even keep playing
-    if checkvollesbrett(brett.copy()):
-        print("DRAW")
-        return 0, None, None  # MIN
-    if checktictactoe(brett.copy(), player):
-        print("WINNER")
-        return 1, None, None  # MAX
-    if checktictactoe(brett.copy(), -player):
-        print("LOSER")
-        return -1, None, None  # MIN
-
-    # If you can make a move, try all of them!
-    minimaxreturnvalues = []
-    zuge = []
-    for zeile in range(len(brett)):
-        for spalte in range(len(brett[zeile])):
-            if brett[zeile][spalte] == 0:
-                brett_weiter = brett.copy()
-                brett_weiter[zeile][spalte] = player
-                minimaxreturnvalues.append(minimax(brett_weiter.copy(), -player))
-                zuge.append((zeile, spalte))
-
-    meinwert = sum([i[0] for i in minimaxreturnvalues])
-    max_wert = max([i[0] for i in minimaxreturnvalues])
-    meine_zeile = None
-    meine_spalte = None
-    for i in range(len(minimaxreturnvalues)):
-        if minimaxreturnvalues[i][0] == max_wert:
-            meine_zeile = zuge[i][0]
-            meine_spalte = zuge[i][1]
-            break
-        else:
-            meine_zeile = 69
-            meine_spalte = 69
-
-    return meinwert, meine_zeile, meine_spalte
-
-'''
-# ----------------------------------------------------------------------------------------------------------------------
 
 
 class minimaxer:
@@ -104,9 +49,9 @@ class minimaxer:
         for zeile in range(len(brett)):
             for spalte in range(len(brett[zeile])):
                 if brett[zeile][spalte] == 0:
-                    brett_weiter = brett.copy()
+                    brett_weiter = copy.deepcopy(brett)
                     brett_weiter[zeile][spalte] = player
-                    minimaxreturnvalues.append(self.minimax(brett_weiter.copy(), -player, recursion+1))
+                    minimaxreturnvalues.append(self.minimax(brett_weiter, -player, recursion+1))
                     print(brett_weiter)
                     n += 1
                     zuge.append((zeile, spalte))
@@ -146,51 +91,5 @@ class minimaxer:
                 if brett[i][j] == 0:
                     return False
         return True
-
-# ----------------------------------------------------------------------------------------------------------------------
-
-'''
-spielbrett = [[0, -1, 0],
-              [0, 1, -1],
-              [1, -1, 1]]
-
-# Oben links, oben rechts   W
-# Oben links, unten links   L
-
-# Oben rechts, oben links   W
-# Oben rechts, unten links  L
-
-# Unten links, oben links   L
-# Unten links, oben rechts  L
-
-
-#print(minimax(spielbrett.copy(), -1))
-
-aaa = minimaxer(spielbrett, -1)
-print("class")
-print(aaa.wert, aaa.zeile, aaa.spalte)
-print("/class")
-
-spielbrett = [[0, -1, -1],
-              [0, 1, 1],
-              [1, -1, 1]]
-
-#print(minimax(spielbrett.copy(), -1))
-
-aaa = minimaxer(spielbrett, -1)
-print("class")
-print(aaa.wert, aaa.zeile, aaa.spalte)
-print("/class")
-'''
-
-spielbrett = [[0, -1, -1],
-              [0, 1, 1],
-              [1, -1, -1]]
-#print(minimax(spielbrett.copy(), -1))
-
-aaa = minimaxer(spielbrett, -1)
-print("class")
-print(aaa.wert, aaa.zeile, aaa.spalte)
-print("/class")
 
 
--- ttt.py
+++ ttt.py
@@ -2,6 +2,8 @@ import numpy as np
 import sys, random
 import pygame
 from anytree import Node, RenderTree
+from minmax import *
+import copy
 sys.setrecursionlimit(255168)
 node = Node("origin")
 nodes = [node]
@@ -18,113 +20,6 @@ done = False
 einheitsmatrix = [[1,0,0],[0,1,0],[0,0,1]]
 einheitsmatrix_transp = [[0,0,1],[0,1,0],[1,0,0]]
 
-
-def checktictactoe(spielbrett1, player):
-    spielbrett_temp = spielbrett1.copy()
-    spielbrett1_transp = np.matrix.transpose(np.asarray(spielbrett_temp))
-    for reihe in range(len(spielbrett_temp)):
-        if np.dot(spielbrett_temp[reihe], np.ones((3, 1))) == player * 3 or np.dot(spielbrett1_transp[reihe], np.ones((3, 1))) == player * 3:
-            return True
-    if spielbrett_temp[0][0] == player and spielbrett_temp[1][1] == player and spielbrett_temp[2][2] == player:
-        return True
-    if spielbrett_temp[0][2] == player and spielbrett_temp[1][1] == player and spielbrett_temp[2][0] == player:
-        return True
-    return False
-
-def checkvollesbrett(brett):
-    for i in range(len(brett)):
-        for j in range(len(brett[i])):
-            if brett[i][j] == 0:
-                return False
-    return True
-
-
-'''
-def minimax(spielbrett1, player, depth=0):
-    spielbr = spielbrett1
-    spielbr_weiter = spielbr.copy()
-    zug_reihe = []
-    zug_stelle = []
-    possibilities = []
-    possibilities_values = []
-    possibilities_bretter = []
-
-    for reihe in range(len(spielbr)):
-        for spalte in range(len(spielbr[reihe])):
-            if spielbr[reihe][spalte] == 0:
-                spielbr_weiter[reihe][spalte] = player
-                if checktictactoe(spielbr_weiter.copy(), -1):
-                    nodee = Node(str(spielbr_weiter))
-                    nodee.parent = nodes[len(nodes) - 1]
-                    nodes.append(nodee)
-                    print("test1 "+str(depth))
-                    return [10, spielbr_weiter, reihe, spalte]
-                if checktictactoe(spielbr_weiter.copy(), 1):
-                    nodee = Node(str(spielbr_weiter))
-                    nodee.parent = nodes[len(nodes) - 1]
-                    nodes.append(nodee)
-                    print("test2 "+str(depth))
-                    return [-1, spielbr_weiter, reihe, spalte]
-                if checkvollesbrett(spielbr_weiter):
-                    nodee = Node(str(spielbr_weiter))
-                    nodee.parent = nodes[len(nodes) - 1]
-                    nodes.append(nodee)
-                    print("test3 "+str(depth))
-                    return [0, spielbr_weiter, reihe, spalte]
-                temp = minimax(spielbr_weiter.copy(), -player, depth+1)
-                possibilities.append(temp)
-                possibilities_values.append(temp[0])
-                possibilities_bretter.append(temp[1])
-                zug_reihe.append(reihe)
-                zug_stelle.append(spalte)
-                print("test4 "+str(depth))
-
-
-    print("test5 "+str(depth))
-    max_value = max(possibilities_values)
-    sum_values = sum(possibilities_values)
-    max_brett = possibilities_bretter[possibilities_values.index(max_value)]
-    max_reihe = zug_reihe[possibilities_values.index(max_value)]
-    max_stelle = zug_stelle[possibilities_values.index(max_value)]
-    nodee = Node(str(max_brett))
-    nodee.parent = nodes[len(nodes)-1]
-    nodes.append(nodee)
-    return [sum_values, max_brett, max_reihe, max_stelle]
-'''
-
-def minimax(brett, player):
-    # Check if you can even keep playing
-    if checkvollesbrett(brett.copy()):
-        return -1, None, None  # MIN
-    if checktictactoe(brett.copy(), player):
-        return 1, None, None  # MAX
-    if checktictactoe(brett.copy(), -player):
-        return -1, None, None  # MIN
-
-    # If you can make a move, try all of them!
-    minimaxreturnvalues = []
-    zuge = []
-    for zeile in range(len(brett)):
-        for spalte in range(len(brett[zeile])):
-            if brett[zeile][spalte] == 0:
-                brett_weiter = brett.copy()
-                brett_weiter[zeile][spalte] = player
-                minimaxreturnvalues.append(minimax(brett_weiter.copy(), -player))
-                zuge.append((zeile, spalte))
-    meinwert = sum([i[0] for i in minimaxreturnvalues])
-    max_wert = max([i[0] for i in minimaxreturnvalues])
-    meine_zeile = None
-    meine_spalte = None
-    for i in range(len(minimaxreturnvalues)):
-        if minimaxreturnvalues[i][0] == max_wert:
-            meine_zeile = zuge[i][0]
-            meine_spalte = zuge[i][1]
-            break
-        else:
-            meine_zeile = 69
-            meine_spalte = 69
-
-    return meinwert, meine_zeile, meine_spalte
 
 def drawboard():
 
@@ -173,6 +68,8 @@ drawboard()
 
 pygame.display.flip()
 
+
+
 while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -206,7 +103,8 @@ while not done:
                 done = True
 
             # KI
-            ai_value, ai_reihe, ai_stelle = minimax(spielbrett_save1.copy(), -1)
+            minimaxe = minimaxer(copy.deepcopy(spielbrett_save1), -1)
+            ai_value, ai_reihe, ai_stelle = minimaxe.wert, minimaxe.zeile, minimaxe.spalte
 
             print("AI moves to: {}/{}".format(ai_reihe, ai_stelle))
             spielbrett[ai_reihe][ai_stelle] = -1
@@ -236,3 +134,11 @@ while not done:
             pygame.display.flip()
             print(spielbrett)
             print(RenderTree(nodes[0]))
+
+
+# Exit loop
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN or event.type == pygame.QUIT:
+            sys.exit(0)
